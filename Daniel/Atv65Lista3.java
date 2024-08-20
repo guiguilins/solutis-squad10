e6fabd53f071064ec8a05afdcc565f9de97f084c
unknown <danisantos5075@gmail.com>
2024-08-20 18:04:28 -0300

    feat: atv61 - atv65

---
+++ Daniel/Atv65Lista3.java
@@ -0,0 +1,24 @@
+package Daniel;
+
+import java.util.Scanner;
+
+public class Atv65Lista3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de linhas:");
+        int linhas = scanner.nextInt();
+
+        for (int i = 1; i <= linhas; i++) {
+            for (int j = 1; j <= linhas - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
