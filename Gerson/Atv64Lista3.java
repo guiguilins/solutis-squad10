fc53648ec5bdcc14f3044a169ae588fe29ca9455
gerson.fragoso <gersonfragoso.araujo@gmail.com>
2024-08-20 18:44:17 -0300

    feat: atvs 61 to 65

---
+++ Gerson/Atv64Lista3.java
@@ -0,0 +1,23 @@
+package Gerson;
+
+import java.util.Scanner;
+
+public class Atv64Lista3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de triângulos: ");
+        int linhas = scanner.nextInt();
+
+        for (int i = 1; i <= linhas; i++) {
+            for (int j = 1; j <= i; j++) {
+                for (int k = 1; k <= j; k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
